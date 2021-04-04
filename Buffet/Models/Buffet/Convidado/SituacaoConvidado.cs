0aed950aac5a1552537913a6de820dc508eb9d64
Bernardo Zacaron <bernardo.zacaron@viannasempre.com.br>
2021-04-04 13:16:36 -0300

    Criação das classes

---
+++ Buffet/Models/Buffet/Convidado/SituacaoConvidado.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Buffet.Models.Buffet.Cliente;
+
+namespace Buffet.Models.Buffet
+{
+    public class SituacaoConvidado
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ Buffet/Models/Buffet/Evento/SituacaoEvento.cs
@@ -0,0 +1,8 @@
+﻿namespace Buffet.Models.Buffet
+{
+    public class SituacaoEvento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; } //Agendado, Cancelado ou Executando
+    }
+}
